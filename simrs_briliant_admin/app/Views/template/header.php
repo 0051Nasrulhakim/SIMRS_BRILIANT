101ebd47be3338760ebd2ac6ccf66d3c8334d5d2
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-06 21:34:09 +0700

    update header

--- simrs_briliant_admin/app/Views/template/header.php
+++ simrs_briliant_admin/app/Views/template/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
+        <title>SIMRS_BRILIANT</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" media="screen" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
